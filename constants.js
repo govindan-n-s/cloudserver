e289ef5a69c7b88c940fdcb9601e1bd65e89d141
gn023184 <govindan.neelagiri-subramanian@broadcom.com>
2020-06-14 23:47:46 +0530

    Initial changes to attempt the backend integration for create bucket

--- constants.js
+++ constants.js
@@ -115,7 +115,7 @@ const constants = {
     objectLocationConstraintHeader: 'x-amz-meta-scal-location-constraint',
     legacyLocations: ['sproxyd', 'legacy'],
     /* eslint-disable camelcase */
-    externalBackends: { aws_s3: true, azure: true, gcp: true },
+    externalBackends: { aws_s3: true, azure: true, gcp: true,hcl_backend: true },
     // some of the available data backends  (if called directly rather
     // than through the multiple backend gateway) need a key provided
     // as a string as first parameter of the get/delete methods.
